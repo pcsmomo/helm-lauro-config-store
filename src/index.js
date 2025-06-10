20af30d42eeaa31de762995c86260c6f717c73ae
Noah Kim <dongwoo_333@hanmail.net>
2025-06-11 08:48:18 +1000

    58. PostgreSQL setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const db = require("./db");
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -12,6 +13,18 @@ app.route("/").get((req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+db.authenticate()
+  .then(() => {
+    console.log("DB connection has been established successfully.");
+    return db.sync();
+  })
+  .then(() => {
+    console.log("DB synchronized and tables created, start server.");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to connect to DB");
+    console.error(err);
+  });
